1217505dbd8759480d4cc5c9c70b9fb95cd5f551
Eskinder-Tk <easkndrtk@gmail.com>
2026-03-13 19:14:23 +0300

    Add Part 1 exercises 1.1 - 1.5

---
+++ Part1/course_info/src/Content.jsx
@@ -0,0 +1,27 @@
+function Part (props) {
+
+    return (
+        <>
+            <p>
+                {props.course.parts[0].name} {props.course.parts[0].exercises}
+            </p>
+            <p>
+                {props.course.parts[1].name} {props.course.parts[1].exercises}
+            </p>
+            <p>
+                {props.course.parts[2].name} {props.course.parts[2].exercises}
+            </p>
+        </>
+    )
+}
+
+
+export default function Contents (props) {
+
+    return (
+        <>
+          <Part course = {props.course} />
+        </>
+    )
+
+}
---
+++ Part1/course_info/src/Total.jsx
@@ -0,0 +1,9 @@
+export default function Total (props) {
+
+    return (
+        <>
+            <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+        </>
+    )
+
+}
